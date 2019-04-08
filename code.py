6c827098c3b6a926697377f85db8c95d1754c432
Oumou Kalsoum <45879230+Barhamienne@users.noreply.github.com>
2019-04-08 13:46:25 +0000

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import os, sys
+
+
+x=input("saisir un premier entier")
+y=input("saisir un deuxième entier")
+M=(int(x)+int(y))/2
+print("La moyenne de %d et %d est de %f" %(x,y,M))  
+
+
